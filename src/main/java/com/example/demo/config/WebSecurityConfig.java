5e11765a7d2216038315e509287d3933adf891f8
rambe98 <rbgks33@gmail.com>
2024-10-21 12:32:34 +0900

    CICD추가

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -49,8 +49,8 @@ public class WebSecurityConfig {
    @Bean
    public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-    		"http://react-developer-env.eba-ah492r9c.ap-northeast-2.elasticbeanstalk.com/")); // 프론트엔드 주소
+      configuration.setAllowedOrigins(Arrays.asList("http://app.han-portfolio.site",
+    		"http://localhost:3000","https://app.han-portfolio.site")); // 프론트엔드 주소
       configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
       configuration.setAllowedHeaders(Arrays.asList("*"));
       configuration.setAllowCredentials(true);
